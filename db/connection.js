627fbb0b2a9db6a9735d85ca7b2beb1d6cbaf394
cip170 <charlottephilipps@gmail.com>
2023-09-11 22:04:31 +1000

    adding syntax erros

--- db/connection.js
+++ db/connection.js
@@ -7,4 +7,12 @@ const db = mysql.createConnection({
     database: 'employee_tracker_db'
 });
 
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database: ' + err.stack);
+        return;
+    }
+    console.log('Connected to the database.');
+});
+
 module.exports = db;
--- db/seeds.sql
+++ db/seeds.sql
@@ -12,7 +12,7 @@ INSERT INTO role
 VALUES
 ('Engineer', 90000, 3),
 ('Accountant', 150000, 2),
-('Operations Director', 220000, 1)
+('Operations Director', 220000, 1),
 ('Lawyer', 200000, 4),
 ('IT Specialist', 70000, 3),
 ('Sales Manager', 70000, 5),
@@ -21,15 +21,15 @@ VALUES
 INSERT INTO employee
 (first_name, last_name, role_id, manager_id)
 VALUES
-('Klaus', 'Monroe', 3, NULL)
-('Harold', 'Jones', 2, NULL)
-('Mick', 'Ronsen', 2, 3)
-('Harry', 'Sullivan', 1, 1)
-('Tracy', 'Mackay', 4, NULL)
-('Regan', 'Casey', 4, Null)
-('Trevor', 'Nielson', 4, 2)
-('Sui', 'Ming', 3, NULL)
-('Alex', 'Porter', 5, 3)
-('Frankie', 'Simm', 6, NULL)
-('Chloe', 'Sangster', 6, NULL)
-('Ross', 'Matthews', 6, NULL)
+('Klaus', 'Monroe', 3, NULL),
+('Harold', 'Jones', 2, NULL),
+('Mick', 'Ronsen', 2, 3),
+('Harry', 'Sullivan', 1, 1),
+('Tracy', 'Mackay', 4, NULL),
+('Regan', 'Casey', 4, Null),
+('Trevor', 'Nielson', 4, 2),
+('Sui', 'Ming', 3, NULL),
+('Alex', 'Porter', 5, 3),
+('Frankie', 'Simm', 6, NULL),
+('Chloe', 'Sangster', 6, NULL),
+('Ross', 'Matthews', 6, NULL),
